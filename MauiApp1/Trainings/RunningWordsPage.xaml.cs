6eca3aa1a364108ffcdf5cfe4bc02c3a5c9e38a3
EgorMak1 <31992160+EgorMak1@users.noreply.github.com>
2026-04-06 18:45:12 +0700

    Добавлены базовые поля и состояние упражнения Бегущие слова

--- MauiApp1/Trainings/RunningWordsPage.xaml.cs
+++ MauiApp1/Trainings/RunningWordsPage.xaml.cs
@@ -2,8 +2,43 @@ namespace MauiApp1.Trainings;
 
 public partial class RunningWordsPage : ContentPage
 {
+    private List<string> _words = new();
+    private List<string> _answerOptions = new();
+
+    private string _correctAnswer = string.Empty;
+    private string _selectedAnswer = string.Empty;
+
+    private int _difficultyLevel = 1;
+    private int _wordDisplayMilliseconds = 1000;
+
+    private bool _isShowingWords = false;
+    private bool _isAnswerSelection = false;
+    private bool _isExerciseFinished = false;
+
     public RunningWordsPage()
     {
         InitializeComponent();
+        InitializeExerciseData();
+    }
+
+    private void InitializeExerciseData()
+    {
+        _words = new List<string>
+        {
+            "дом",
+            "лес",
+            "река",
+            "мост"
+        };
+
+        _answerOptions = new List<string>
+        {
+            "река",
+            "стол",
+            "окно",
+            "мяч"
+        };
+
+        _correctAnswer = "река";
     }
 }
